fbfb9ce3e2438182d0f61e8096641c88fe7e16cf
vansky <6255296+vansky@users.noreply.github.com>
2021-11-05 15:12:17 -0400

    mapper works

--- map_to_mb-natstor.sh
+++ map_to_mb-natstor.sh
@@ -38,7 +38,7 @@ cat genmodel/naturalstories.penn.linetrees | python ${mb_location}/resource-natu
 
 ### combine with measures
 echo "Combine words and measures"
-echo 'words' > ${natstor_words}
+echo 'word' > ${natstor_words}
 sed 's/ /\n/g' genmodel/naturalstories.linetoks >> ${natstor_words}
 paste -d' ' ${natstor_words} ${df_full} > ${df_full}.words
 python ${unigram_training_script} < ${unigram_data} > ${unigram_file}
@@ -54,7 +54,7 @@ paste -d' ' <(cat ${naturalstories_dir}/naturalstories_RTS/all_stories.tok | sed
 
 echo "Combine with RTs"
 ### combine with RTs
-paste -d' ' ${natstor_words} <(cut -d' ' -f2- ${df_full}.unigrams) | python ${mb_location}/resource-rt/scripts/roll_toks.py <(sed 's/(/-LRB-/g;s/)/-RRB-/g;' genmodel/naturalstories.mfields.itemmeasures) sentid sentpos > ${df_full}.models
+paste -d' ' ${natstor_words} <(python ${mb_location}/resource-rt/scripts/filter_cols.py -x 'word' < ${df_full}.unigrams) | python ${mb_location}/resource-rt/scripts/roll_toks.py <(sed 's/(/-LRB-/g;s/)/-RRB-/g;' genmodel/naturalstories.mfields.itemmeasures) sentid sentpos > ${df_full}.models
 
 cut -d' ' -f2- ${df_full}.models  | paste -d' ' genmodel/naturalstories.mfields.itemmeasures - > ${df_full}.models_mergable
 
