5264c03d267067c8c99dcdda885e8804b3c2e0c2
vansky <6255296+vansky@users.noreply.github.com>
2021-11-05 00:38:09 -0400

    further

--- map_to_mb-natstor.sh
+++ map_to_mb-natstor.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-naturalstories_dir=X
+naturalstories_dir=natstor
+scripts_dir=../replications/vanschijndel_linzen-2019-scil
 
 ## Required paths
 ### modelblocks location
 mb_location=../modelblocks-release
 
 ### unigram scripts (e.g., from replication repo, SCIL 2019)
-unigram_training_script=scripts/calcunigram.py
-unigram_insertion_script=scripts/insertunigrams.py
-roll_removal_script=scripts/removerolled.py
+unigram_training_script=${scripts_dir}/scripts/calcunigram.py
+unigram_insertion_script=${scripts_dir}/scripts/insertunigrams.py
+roll_removal_script=${scripts_dir}/scripts/removerolled.py
 ### unigram training data
 unigram_data=wikitext-103/wiki.train.tokens
 ### RT data from RT corpus
@@ -17,18 +18,18 @@ rt_data=processed_RTs.tsv
 
 ## Generated filenames
 ### column of tokenized input words
-natstor_words=natstor.toks
+natstor_words=genmodel/natstor.toks
 ### all complexity measures combined
-df_full=naturalstories.full.results
-### complexity combined with input words
-df_fullwords=naturalstories.results.fullwords
-
-
-
-python model_paster.py ${df_full} "$@"
+df_full=genmodel/naturalstories.full.results
+### unigram file
+unigram_file=genmodel/unigram.toks
 
 mkdir genmodel
 
+echo "Combine measures"
+python model_paster.py ${df_full} "$@"
+
+echo "Make linetoks"
 ### make %linetoks
 cat ${naturalstories_dir}/parses/penn/all-parses.txt.penn | perl ${mb_location}/resource-linetrees/scripts/editabletrees2linetrees.pl > genmodel/naturalstories.penn.linetrees  
 
@@ -36,19 +37,23 @@ cat genmodel/naturalstories.penn.linetrees | python ${mb_location}/resource-natu
 
 
 ### combine with measures
+echo "Combine words and measures"
 echo 'words' > ${natstor_words}
 sed 's/ /\n/g' genmodel/naturalstories.linetoks >> ${natstor_words}
 paste -d' ' ${natstor_words} ${df_full} > ${df_full}.words
-python ${unigram_training_script} < ${unigram_data} > unigrams.txt
-python ${unigram_insertion_script} unigrams.txt ${df_full}.words > ${df_full}.unigrams
+python ${unigram_training_script} < ${unigram_data} > ${unigram_file}
+python ${unigram_insertion_script} ${unigram_file} ${df_full}.words > ${df_full}.unigrams
 
+echo "Making mfields"
 ### make genmodel/naturalstories.mfields.itemmeasures
 cat ${naturalstories_dir}/naturalstories_RTS/all_stories.tok | sed 's/\t/ /g;s/``/'\''/g;s/'\'\''/'\''/g;s/(/-LRB-/g;s/)/-RRB-/g;s/peaked/peeked/g;' | python ${mb_location}/resource-rt/scripts/toks2sents.py genmodel/naturalstories.linetoks > genmodel/naturalstories.lineitems  
 
-paste -d' ' <(cat ${naturalstories_dir}/naturalstories_RTS/all_stories.tok | sed 's/\t/ /g;s/peaked/peeked/g') <(cat genmodel/naturalstories.lineitems | python ${mb_location}/resource-rt/scripts/sents2sentids.py | cut -d' ' -f 2-) \  
-    <(cat ${naturalstories}/naturalstories_RTS/all_stories.tok | sed 's/\t/ /g;' | awk -f ${mb_location}/resource-rt/scripts/filter_cols.awk -v cols=item - | python ${mb_location}/resource-rt/scripts/rename_cols.py item docid) > genmodel/naturalstories.mfields.itemmeasures  
-rm genmodel/naturalstories.lineitems  
+echo "  Part 2"
+paste -d' ' <(cat ${naturalstories_dir}/naturalstories_RTS/all_stories.tok | sed 's/\t/ /g;s/peaked/peeked/g') <(cat genmodel/naturalstories.lineitems | python ${mb_location}/resource-rt/scripts/sents2sentids.py | cut -d' ' -f 2-) | \
+    paste -d' ' - <(cat ${naturalstories_dir}/naturalstories_RTS/all_stories.tok | sed 's/\t/ /g;' | awk -f ${mb_location}/resource-rt/scripts/filter_cols.awk -v cols=item - | python ${mb_location}/resource-rt/scripts/rename_cols.py item docid) > genmodel/naturalstories.mfields.itemmeasures  
+#rm genmodel/naturalstories.lineitems  
 
+echo "Combine with RTs"
 ### combine with RTs
 paste -d' ' ${natstor_words} <(cut -d' ' -f2- ${df_full}.unigrams) | python ${mb_location}/resource-rt/scripts/roll_toks.py <(sed 's/(/-LRB-/g;s/)/-RRB-/g;' genmodel/naturalstories.mfields.itemmeasures) sentid sentpos > ${df_full}.models
 
@@ -60,5 +65,5 @@ python ${mb_location}/resource-rt/scripts/rm_unfix_items.py < ${df_full}.models_
 
 python ${roll_removal_script} < ${df_full}.models_rt_filtered > ${df_full}.models_rt_filtered_noroll 
 
-mkdir rdata  
-mkdir results  
+#mkdir rdata  
+#mkdir results  
