9f3d2706279bc6f476dbe5ac75bee75a50ac9947
Marten van Schijndel <vansky@users.noreply.github.com>
2021-11-10 17:46:54 -0500

    generalized to arithmetic ops

--- scripts/diffMetrics.py
+++ scripts/diffMetrics.py
@@ -1,12 +1,21 @@
-import sys, argparse, pandas as pd
+import sys, argparse, operator, pandas as pd
 argparser = argparse.ArgumentParser('''
-Diffs user-specified columns in space-delimited data table.
+Combines user-specified columns in space-delimited data table.
 ''')
-argparser.add_argument('-c', '--columns', dest='c', nargs='+', action='store', help='Columns to diff. Format: -c col1,col2 col3,col4 -> col1-col2 col3-col4')
+argparser.add_argument('-c', '--columns', dest='c', nargs='+', action='store', help='Columns to combine. Format: -c col1,col2 col3,col4 (with -f=-)-> col1-col2 col3-col4')
+argparser.add_argument('-f', '--function', dest='f', action='store', default='-', help='Function with which to combine columns.')
 args, unknown = argparser.parse_known_args()
+
+def function_parser(func_string):
+    func_dict = {'+':operator.add,
+                 '-':operator.sub,
+                 '*':operator.mul,
+                 '/':operator.truediv,
+                 '%':operator.mod}
+    return(func_dict[func_string])
 
 data = pd.read_csv(sys.stdin, sep=' ', skipinitialspace=True)
 for colpair in args.c:
     col1,col2 = colpair.split(',')
-    data['diff:'+str(col1)+'_'+str(col2)] = data[col1] - data[col2]
+    data[str(col1)+str(args.f)+str(col2)] = function_parser(args.f)(data[col1],data[col2])
 data.to_csv(sys.stdout, sep=' ', index=False, na_rep='nan')
