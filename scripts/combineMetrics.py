62f8e8fc0dd746a0ee070c7dc32ec68018bcb324
Marten van Schijndel <vansky@users.noreply.github.com>
2021-11-10 17:58:49 -0500

    mixed combinations

--- scripts/combineMetrics.py
+++ scripts/combineMetrics.py
@@ -2,8 +2,7 @@ import sys, argparse, operator, pandas as pd
 argparser = argparse.ArgumentParser('''
 Combines user-specified columns in space-delimited data table.
 ''')
-argparser.add_argument('-c', '--columns', dest='c', nargs='+', action='store', help='Columns to combine. Format: -c col1,col2 col3,col4 (with -f=-)-> col1-col2 col3-col4')
-argparser.add_argument('-f', '--function', dest='f', action='store', default='-', help='Function with which to combine columns.')
+argparser.add_argument('-f', '--columns', dest='f', nargs='+', action='store', help='Columns to combine. Format: -f col1,-,col2 col3,+,col4 -> col1-col2 col3+col4')
 args, unknown = argparser.parse_known_args()
 
 def function_parser(func_string):
@@ -15,7 +14,7 @@ def function_parser(func_string):
     return(func_dict[func_string])
 
 data = pd.read_csv(sys.stdin, sep=' ', skipinitialspace=True)
-for colpair in args.c:
-    col1,col2 = colpair.split(',')
-    data[str(col1)+str(args.f)+str(col2)] = function_parser(args.f)(data[col1],data[col2])
+for colpair in args.f:
+    col1,op,col2 = colpair.split(',')
+    data[str(col1)+str(op)+str(col2)] = function_parser(op)(data[col1],data[col2])
 data.to_csv(sys.stdout, sep=' ', index=False, na_rep='nan')
